1a35cb14db25374c5d69a53093d4f058d9a903e9
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-12-27 23:04:17 +0100

    refactor: improve the layout of the header from Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/30758309?v=4",
+              }}
+            />
+            <User>
+              <UserGreetings>Olá,</UserGreetings>
+              <UserName>Rodrigo</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
